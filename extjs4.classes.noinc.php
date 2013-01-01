07c68566c1582b1b1b50152a03c45ac296df156f
Tom Schaefer <thomas.schaefer@query4u.de>
2013-01-01 06:54:43 -0800

    Create extjs4.classes.noinc.php
    classes for IDE autocompleter support

---
+++ extjs4.classes.noinc.php
@@ -0,0 +1,316 @@
+<?php 
+
+class extjs4_app_Application {
+  public $appFolder;
+	public $autoCreateViewport;
+	public $controllers;
+	public $enableQuickTips;
+	public $id;
+	public $listeners;
+	public $models;
+	public $name;
+	public $paths;
+	public $refs;
+	public $scope;
+	public $stores;
+	public $views;
+	public function addEvents(){}
+	public function addListener(){}
+	public function addManagedListener(){}
+	public function addMembers(){}
+	public function addRef(){}
+	public function addStatics(){}
+	public function clearListeners(){}
+	public function clearManagedListeners(){}
+	public function control(){}
+	public function create(){}
+	public function createAlias(){}
+	public function enableBubble(){}
+	public function fireEvent(){}
+	public function getController(){}
+	public function getInitialConfig(){}
+	public function getModel(){}
+	public function getName(){}
+	public function getStore(){}
+	public function getView(){}
+	public function hasListener(){}
+	public function hasRef(){}
+	public function init(){}
+	public function launch(){}
+	public function mon(){}
+	public function mun(){}
+	public function on(){}
+	public function onLaunch(){}
+	public function relayEvents(){}
+	public function removeListener(){}
+	public function removeManagedListener(){}
+	public function resumeEvents(){}
+	public function suspendEvents(){}
+	public function un(){}
+}
+class extjs4_app_Controller {
+}
+class extjs4_container_ButtonGroup {
+}
+class extjs4_container_Container {
+}
+class extjs4_container_DockingContainer {
+}
+class extjs4_container_Viewport {
+}
+class extjs4_data_ArrayStore {
+}
+class extjs4_data_Batch {
+}
+class extjs4_data_Connection {
+}
+class extjs4_data_DirectStore {
+}
+class extjs4_data_Errors {
+}
+class extjs4_data_Field {
+}
+class extjs4_data_IdGenerator {
+}
+class extjs4_data_JsonPStore {
+}
+class extjs4_data_JsonStore {
+}
+class extjs4_data_Model {
+}
+class extjs4_data_Operation {
+}
+class extjs4_app{
+	/**
+	 * @return extjs4_app_Application
+	 */
+	public function Application(){}
+	/**
+	 * @return extjs4_app_Controller
+	 */
+	public function Controller(){}
+}
+class extjs4_chart{
+}
+class extjs4_container{
+	/**
+	 * @return extjs4_container_ButtonGroup
+	 */
+	public function ButtonGroup(){}
+	/**
+	 * @return extjs4_container_Container
+	 */
+	public function Container(){}
+	/**
+	 * @return extjs4_container_DockingContainer
+	 */
+	public function DockingContainer(){}
+	/**
+	 * @return extjs4_container_Viewport
+	 */
+	public function Viewport(){}
+}
+class extjs4_data{
+	/**
+	 * @return extjs4_data_ArrayStore
+	 */
+	public function ArrayStore(){}
+	/**
+	 * @return extjs4_data_Batch
+	 */
+	public function Batch(){}
+	/**
+	 * @return extjs4_data_Connection
+	 */
+	public function Connection(){}
+	/**
+	 * @return extjs4_data_DirectStore
+	 */
+	public function DirectStore(){}
+	/**
+	 * @return extjs4_data_Errors
+	 */
+	public function Errors(){}
+	/**
+	 * @return extjs4_data_Field
+	 */
+	public function Field(){}
+	/**
+	 * @return extjs4_data_IdGenerator
+	 */
+	public function IdGenerator(){}
+	/**
+	 * @return extjs4_data_JsonPStore
+	 */
+	public function JsonPStore(){}
+	/**
+	 * @return extjs4_data_JsonStore
+	 */
+	public function JsonStore(){}
+	/**
+	 * @return extjs4_data_Model
+	 */
+	public function Model(){}
+	/**
+	 * @return extjs4_data_Operation
+	 */
+	public function Operation(){}
+}
+class extjs4_dd{
+}
+class extjs4_direct{
+}
+class extjs4_dom{
+}
+class extjs4_draw{
+}
+class extjs4_flash{
+}
+class extjs4_form{
+}
+class extjs4_fx{
+}
+class extjs4_grid{
+}
+class extjs4_layout{
+}
+class extjs4_menu{
+}
+class extjs4_panel{
+}
+class extjs4_picker{
+}
+class extjs4_selection{
+}
+class extjs4_slider{
+}
+class extjs4_state{
+}
+class extjs4_tab{
+}
+class extjs4_tip{
+}
+class extjs4_tree{
+}
+class extjs4_toolbar{
+}
+class extjs4_util{
+}
+class extjs4_ux{
+}
+class extjs4_view{
+}
+class extjs4_window{
+}
+class extjs4_base {
+	/**
+	 * @var extjs4_app
+	 */
+	public $app;
+	/**
+	 * @var extjs4_chart
+	 */
+	public $chart;
+	/**
+	 * @var extjs4_container
+	 */
+	public $container;
+	/**
+	 * @var extjs4_data
+	 */
+	public $data;
+	/**
+	 * @var extjs4_dd
+	 */
+	public $dd;
+	/**
+	 * @var extjs4_direct
+	 */
+	public $direct;
+	/**
+	 * @var extjs4_dom
+	 */
+	public $dom;
+	/**
+	 * @var extjs4_draw
+	 */
+	public $draw;
+	/**
+	 * @var extjs4_flash
+	 */
+	public $flash;
+	/**
+	 * @var extjs4_form
+	 */
+	public $form;
+	/**
+	 * @var extjs4_fx
+	 */
+	public $fx;
+	/**
+	 * @var extjs4_grid
+	 */
+	public $grid;
+	/**
+	 * @var extjs4_layout
+	 */
+	public $layout;
+	/**
+	 * @var extjs4_menu
+	 */
+	public $menu;
+	/**
+	 * @var extjs4_panel
+	 */
+	public $panel;
+	/**
+	 * @var extjs4_picker
+	 */
+	public $picker;
+	/**
+	 * @var extjs4_selection
+	 */
+	public $selection;
+	/**
+	 * @var extjs4_slider
+	 */
+	public $slider;
+	/**
+	 * @var extjs4_state
+	 */
+	public $state;
+	/**
+	 * @var extjs4_tab
+	 */
+	public $tab;
+	/**
+	 * @var extjs4_tip
+	 */
+	public $tip;
+	/**
+	 * @var extjs4_tree
+	 */
+	public $tree;
+	/**
+	 * @var extjs4_toolbar
+	 */
+	public $toolbar;
+	/**
+	 * @var extjs4_util
+	 */
+	public $util;
+	/**
+	 * @var extjs4_ux
+	 */
+	public $ux;
+	/**
+	 * @var extjs4_view
+	 */
+	public $view;
+	/**
+	 * @var extjs4_window
+	 */
+	public $window;
+	
+	
+}
