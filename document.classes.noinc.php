3e29313b06a3bc6300c7bc14994201de94ca3a55
Tom Schaefer <thomas.schaefer@query4u.de>
2013-01-01 07:01:10 -0800

    Create document.classes.noinc.php
    Some Dom classes for IDE autocompletion support

---
+++ document.classes.noinc.php
@@ -0,0 +1,177 @@
+<?php 
+
+class document_autocompleter {
+  
+	/**
+	 * @return jsDomElement
+	 */
+	public function getElementById($name){}
+	
+	
+}
+
+class jsStyleObject{
+	public $background;
+	public $backgroundAttachment;
+	public $backgroundColor;
+	public $backgroundImage;
+	public $backgroundPosition;
+	public $backgroundRepeat;
+	public $bottom;
+	public $boxShadow;
+	public $border;
+	public $borderRadius;
+	public $borderTopLeftRadius;
+	public $borderTopRightRadius;
+	public $borderBottomLeftRadius;
+	public $borderBottomRightRadius;
+	public $borderCollapse;
+	public $borderColor;
+	public $borderSpacing;
+	public $borderStyle;
+	public $borderTop;
+	public $borderTopStyle;
+	public $borderTopWidth;
+	public $borderTopColor;
+	public $borderLeft;
+	public $borderLeftStyle;
+	public $borderLeftWidth;
+	public $borderLeftColor;
+	public $borderRight;
+	public $borderRightStyle;
+	public $borderRightWidth;
+	public $borderRightColor;
+	public $borderBottom;
+	public $borderBottomStyle;
+	public $borderBottomWidth;
+	public $borderBottomColor;
+	public $color;
+	public $captionSide;
+	public $clear;
+	public $clip;
+	public $clipRule;
+	public $clipPath;
+	public $colorInterpolation;
+	public $colorInterpolationFilters;
+	public $content;
+	public $counterIncrement;
+	public $counterReset;
+	public $cssFloat;
+	public $cssText;
+	public $cursor;
+	public $direction;
+	public $display;
+	public $emptyCells;
+	public $fill;
+	public $fillOpacity;
+	public $fillRule;
+	public $filter;
+	public $floodColor;
+	public $floodOpacity;
+	public $font;
+	public $fontFamily;
+	public $fontSize;
+	public $fontSizeAdjust;
+	public $fontStretch;
+	public $fontStyle;
+	public $fontVariant;
+	public $fontWeight;
+	public $height;
+	public $left;
+	public $letterspacing;
+	public $lineHeight;
+	public $listStyle;
+	public $listStylePosition;
+	public $listStyleType;
+	public $listStyleImage;
+	public $margin;
+	public $marginBottom;
+	public $marginTop;
+	public $marginRight;
+	public $marginLeft;
+	public $maxHeight;
+	public $minHeight;
+	public $minWidth;
+	public $opacity;
+	public $orphans;
+	public $outline;
+	public $outlineColor;
+	public $outlineWidth;
+	public $outlineStyle;
+	public $overflow;
+	public $overflowX;
+	public $overflowY;
+	public $padding;
+	public $paddingBottom;
+	public $paddingTop;
+	public $paddingLeft;
+	public $paddingRight;
+	public $page;
+	public $pageAfterBreak;
+	public $pageBeforeBreak;
+	public $pageInsideBreak;
+	public $position;
+	public $tableLayout;
+	public $textAlign;
+	public $textDecoration;
+	public $textIndent;
+	public $textOverflow;
+	public $textAnchor;
+	public $textRendering;
+	public $textShadow;
+	public $textTransform;
+	public $top;
+	public $verticalAlign;
+	public $visibility;
+	public $whiteSpace;
+	public $width;
+	public $widows;
+	public $wordSpacing;
+	public $wordWrap;
+	public $zIndex;
+}
+class jsDomElement {
+	/**
+	 * @var jsStyleObject
+	 */
+	public $style;
+	/**
+	 * @var string
+	 */
+	public $title;
+	/**
+	 * @var string
+	 */
+	public $id;
+	public $lang;
+	public $dir;
+	public $className;
+	public $tabIndex;
+	public $accessKey;
+	public $accessKeyLabel;
+	public $align;
+	public $vAlign;
+	public $contentEditable;
+	public $isContentEditable;
+	public $innerHTML;
+	public $outerHTML;
+	public $offsetParent;
+	public $offsetTop;
+	public $offsetLeft;
+	public $offsetWidth;
+	public $onclick;
+	public $onmouseover;
+	public $onmouseout;
+	public $onmousedown;
+	public $ondblclick;
+	public $onfocus;
+	public $onblur;
+	public $onchange;
+	
+	public function contains(){}
+	public function querySelector($selector){}
+	public function querySelectorAll($selectors){}
+	public function addEventListener($type, $listener, $useCapture){}
+	public function removeEventListener($type, $listener, $useCapture){}
+
+}
